dfc90dd306741c5881c3816c40c2ba5622685a57
Hans Pabst <hans.pabst@intel.com>
2015-02-21 21:20:02 +0100

    Added README.sh generating an up-to-date PDF within the documentation folder. Git-ignore intermediate template file README.tex. Adjusted README.md to play well with Pandoc's implicit_figures extension.

---
+++ README.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# dump pandoc template for latex
+pandoc -D latex > README.tex
+
+# adjust template
+sed -i \
+  -e 's/\(\\documentclass\[.\+\]{.\+}\)/\1\n\\pagenumbering{gobble}/' \
+  -e 's/\\usepackage{listings}/\\usepackage{listings}\\lstset{basicstyle=\\footnotesize\\ttfamily}/' \
+  README.tex
+
+# cleanup markup and pipe into pandoc using the template
+sed \
+  -e 's/\[\[.\+\](.\+)\]//' \
+  -e '/!\[.\+\](.\+)/{n;d}' \
+  README.md | \
+pandoc \
+  --template=README.tex --listings \
+  -f markdown_github+implicit_figures \
+  -V documentclass=scrartcl \
+  -V classoption=DIV=19 \
+  -o documentation/libxstream.pdf
