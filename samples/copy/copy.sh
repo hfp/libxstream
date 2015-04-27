0616999dbe69375166acc378e750f53545fba824
Hans Pabst <hans.pabst@intel.com>
2015-04-27 13:58:57 +0200

    Generate performance plot.

--- samples/copy/copy.sh
+++ samples/copy/copy.sh
@@ -4,18 +4,30 @@ HERE=$(cd $(dirname $0); pwd -P)
 NAME=$(basename ${HERE})
 
 if [[ "-mic" != "$1" ]] ; then
+  if [[ "$1" == "i"* ]] ; then
+    FILE=copyin.dat
+  else
+    FILE=copyout.dat
+  fi
   env \
     KMP_AFFINITY=scatter,granularity=fine \
     OFFLOAD_INIT=on_start \
     MIC_ENV_PREFIX=MIC \
     MIC_KMP_AFFINITY=scatter,granularity=fine \
-  ${HERE}/${NAME} $*
+  ${HERE}/${NAME} $* | \
+  tee ${FILE}
 else
   shift
+  if [[ "$1" == "i"* ]] ; then
+    FILE=copyin.dat
+  else
+    FILE=copyout.dat
+  fi
   env \
     SINK_LD_LIBRARY_PATH=$MIC_LD_LIBRARY_PATH \
   micnativeloadex \
     ${HERE}/${NAME} -a "$*" \
-    -e "KMP_AFFINITY=scatter,granularity=fine"
+    -e "KMP_AFFINITY=scatter,granularity=fine" | \
+  tee ${FILE}
 fi
 
