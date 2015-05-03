3c3c29e76fc299347e517004e885809a1f6c984e
Hans Pabst <hans.pabst@intel.com>
2015-05-03 10:23:02 +0200

    Code cleanup.

--- src/libxstream_offload.cpp
+++ src/libxstream_offload.cpp
@@ -113,7 +113,7 @@ int libxstream_offload(libxstream_function function, const libxstream_argument s
     LIBXSTREAM_PRINT(2, "fn_call: fn=0x%llx args=0x%llx stream=0x%llx flags=%i",
       reinterpret_cast<unsigned long long>(fhybrid ? fhybrid : reinterpret_cast<libxstream_function>(fnative)),
       reinterpret_cast<unsigned long long>(signature), reinterpret_cast<unsigned long long>(LIBXSTREAM_ASYNC_STREAM), cflags);
-    LIBXSTREAM_PRINT0(3, "***************************************************************************");
+    LIBXSTREAM_PRINT0(3, "************************************************************************");
 
 #if defined(LIBXSTREAM_OFFLOAD)
     if (0 <= LIBXSTREAM_ASYNC_DEVICE) {
