43183be2485c4adf8d4a9e57fd752a16ce202681
Hans Pabst <hans.pabst@intel.com>
2015-05-11 14:30:12 +0200

    Code cleanup and preparation for (optional) hybrid execution. VS: setup project dependency for entropy code sample.

--- samples/entropy/entropy.c
+++ samples/entropy/entropy.c
@@ -9,7 +9,7 @@
 #include <libxstream_end.h>
 
 
-LIBXSTREAM_TARGET(mic) void histogram(const char* data, size_t* histogram)
+LIBXSTREAM_TARGET(mic) void makehist(const char* data, size_t* histogram)
 {
   static const size_t maxint = (size_t)(((unsigned int)-1) >> 1);
   int i, j, m;
@@ -63,7 +63,7 @@ int main(int argc, char* argv[])
 
   size_t filesize = 0;
   FILE *const file = 1 < argc ? fileopen(argv[1], "rb", &filesize) : 0;
-  const size_t nitems = (1 < argc && 0 == filesize && 0 < atoi(argv[1])) ? (atoi(argv[1]) * (1 << 20)/*MB*/) : (0 < filesize ? filesize : (512 << 20));
+  const size_t nitems = (1 < argc && 0 == filesize && 0 < atoi(argv[1])) ? (atoi(argv[1]) * (1ULL << 20)/*MB*/) : (0 < filesize ? filesize : (512 << 20));
   const size_t mbatch = LIBXSTREAM_MIN(2 < argc ? strtoul(argv[2], 0, 10) : 0/*auto*/, nitems);
   const int nstreams = LIBXSTREAM_MIN(LIBXSTREAM_MAX(3 < argc ? atoi(argv[3]) : 2, 1), LIBXSTREAM_MAX_NSTREAMS) * (int)ndevices;
 #if defined(_OPENMP)
@@ -71,14 +71,16 @@ int main(int argc, char* argv[])
 #else
   LIBXSTREAM_PRINT0(1, "OpenMP support needed for performance results!");
 #endif
-  const size_t nbatch = (0 == mbatch) ? (nitems / nstreams) : mbatch;
+  const size_t nbatch = (0 == mbatch) ? (nitems / nstreams) : mbatch, hsize = 256;
+  size_t histogram[256/*hsize*/];
+  memset(histogram, 0, sizeof(histogram));
 
   char* data;
   { /*allocate and initialize host memory*/
     size_t i;
     LIBXSTREAM_CHECK_CALL_ASSERT(libxstream_mem_allocate(-1/*host*/, (void**)&data, nitems, 0));
     if (0 == filesize || nitems > fread(data, 1, filesize, file)) {
-      for (i = 0; i < nitems; ++i) data[i] = LIBXSTREAM_MOD(rand(), 256/*POT*/);
+      for (i = 0; i < nitems; ++i) data[i] = (char)LIBXSTREAM_MOD(rand(), hsize/*POT*/);
     }
   }
 
@@ -100,8 +102,8 @@ int main(int argc, char* argv[])
       const int device = (0 < ndevices) ? ((int)(i % ndevices)) : -1;
       LIBXSTREAM_CHECK_CALL_ASSERT(libxstream_stream_create(&stream[i].handle, device, 0, name));
       LIBXSTREAM_CHECK_CALL_ASSERT(libxstream_mem_allocate(device, (void**)&stream[i].data, nbatch, 0));
-      LIBXSTREAM_CHECK_CALL_ASSERT(libxstream_mem_allocate(device, (void**)&stream[i].histogram, 256 * sizeof(size_t), 0));
-      LIBXSTREAM_CHECK_CALL_ASSERT(libxstream_memset_zero(stream[i].histogram, 256 * sizeof(size_t), stream[i].handle));
+      LIBXSTREAM_CHECK_CALL_ASSERT(libxstream_mem_allocate(device, (void**)&stream[i].histogram, hsize * sizeof(size_t), 0));
+      LIBXSTREAM_CHECK_CALL_ASSERT(libxstream_memset_zero(stream[i].histogram, hsize * sizeof(size_t), stream[i].handle));
     }
 
     /*start benchmark with no pending work*/
@@ -109,7 +111,6 @@ int main(int argc, char* argv[])
   }
 
   /*process data in chunks of size nbatch*/
-  const size_t hsize = 256;
   const int end = (int)((nitems + nbatch - 1) / nbatch);
   int batch;
   libxstream_type sizetype = LIBXSTREAM_TYPE_U32;
@@ -127,18 +128,16 @@ int main(int argc, char* argv[])
     LIBXSTREAM_CHECK_CALL_ASSERT(libxstream_fn_signature(&signature));
     LIBXSTREAM_CHECK_CALL_ASSERT(libxstream_fn_input(signature, 0, stream[i].data, LIBXSTREAM_TYPE_CHAR, 1, &size));
     LIBXSTREAM_CHECK_CALL_ASSERT(libxstream_fn_output(signature, 1, stream[i].histogram, sizetype, 1, &hsize));
-    LIBXSTREAM_CHECK_CALL_ASSERT(libxstream_fn_call((libxstream_function)histogram, signature, stream[i].handle, LIBXSTREAM_CALL_DEFAULT));
+    LIBXSTREAM_CHECK_CALL_ASSERT(libxstream_fn_call((libxstream_function)makehist, signature, stream[i].handle, LIBXSTREAM_CALL_DEFAULT));
   }
 
-  size_t histogram[256];
-  memset(histogram, 0, sizeof(histogram));
   { /*reduce stream-local histograms*/
-    LIBXSTREAM_ALIGNED(size_t local[256], LIBXSTREAM_MAX_SIMD);
+    LIBXSTREAM_ALIGNED(size_t local[256/*hsize*/], LIBXSTREAM_MAX_SIMD);
     int i, j;
     for (i = 0; i < nstreams; ++i) {
       LIBXSTREAM_CHECK_CALL_ASSERT(libxstream_memcpy_d2h(stream[i].histogram, local, sizeof(local), stream[i].handle));
       LIBXSTREAM_CHECK_CALL_ASSERT(libxstream_stream_wait(stream[i].handle)); /*wait for pending work*/
-      for (j = 0; j < 256; ++j) histogram[j] += local[j];
+      for (j = 0; j < hsize; ++j) histogram[j] += local[j];
     }
   }
 
@@ -147,7 +146,7 @@ int main(int argc, char* argv[])
   { /*calculate entropy*/
     const double log2_nitems = log2((double)nitems);
     int i;
-    for (i = 0; i < 256; ++i) {
+    for (i = 0; i < hsize; ++i) {
       const double h = (double)histogram[i];
       entropy -= h * (log2(h) - log2_nitems);
     }
