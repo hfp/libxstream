2f957fdcfae4967cee1a1c8d1166003a600ce75a
Hans Pabst <hans.pabst@intel.com>
2015-06-11 19:10:20 +0200

    Integrated some tests into run scripts.

--- samples/entropy/entropy.sh
+++ samples/entropy/entropy.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
-env \
-  KMP_AFFINITY=scatter \
-  OFFLOAD_INIT=on_start \
-  MIC_USE_2MB_BUFFERS=2m \
-  MIC_ENV_PREFIX=MIC \
-  MIC_KMP_AFFINITY=balanced,granularity=fine \
-./entropy $*
+export KMP_AFFINITY=scatter
+export OFFLOAD_INIT=on_start
+export MIC_USE_2MB_BUFFERS=2m
+export MIC_ENV_PREFIX=MIC
+export MIC_KMP_AFFINITY=balanced,granularity=fine
+
+HERE=$(cd $(dirname $0); pwd -P)
+
+if [[ "-test" == "$1" ]] ; then
+  TESTS=( \
+    "16 1 2 4" \
+    "19 1 4 16" \
+    "45 1 4 4" \
+    "25 1 13 3" \
+    "16 1 2 17" \
+    "45 1 4 16" \
+    "25 1 13 12" \
+  )
+else
+  TESTS=( "$*" )
+fi
+
+for TEST in "${TESTS[@]}" ; do
+  ${HERE}/entropy ${TEST}
+done
+
--- samples/multi-dgemm/multi-dgemm.sh
+++ samples/multi-dgemm/multi-dgemm.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
-env \
-  KMP_AFFINITY=scatter \
-  OFFLOAD_INIT=on_start \
-  MIC_USE_2MB_BUFFERS=2m \
-  MIC_ENV_PREFIX=MIC \
-  MIC_KMP_AFFINITY=balanced,granularity=fine \
-./multi-dgemm $*
+export KMP_AFFINITY=scatter
+export OFFLOAD_INIT=on_start
+export MIC_USE_2MB_BUFFERS=2m
+export MIC_ENV_PREFIX=MIC
+export MIC_KMP_AFFINITY=balanced,granularity=fine
+
+HERE=$(cd $(dirname $0); pwd -P)
+
+if [[ "-test" == "$1" ]] ; then
+  export CHECK=1
+  TESTS=( \
+    "4 1 1 4" \
+    "10 2 2 4" \
+    "14 2 2 4" \
+    "20 1 2 4" \
+    "30 2 2 4" \
+    "40 2 2 4" \
+    "40 1 2 32" \
+    "60 2 4 4" \
+    "128 2 2 4" \
+  )
+else
+  TESTS=( "$*" )
+fi
+
+for TEST in "${TESTS[@]}" ; do
+  ${HERE}/multi-dgemm ${TEST}
+done
+
