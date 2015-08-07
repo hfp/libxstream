bd71a37fd2454f8fbd4cb091464ba663eae3d3b6
Hans Pabst <hans.pabst@intel.com>
2015-08-07 14:29:17 +0200

    Improved benchmark.sh script.

--- samples/multi-dgemm/benchmark.sh
+++ samples/multi-dgemm/benchmark.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
 #TRY="echo"
+
 FILE="benchmark.txt"
-STREAMS=2
 SIZE=250
+BSIZE=16
 STRIDE=1
-FRACT=8
 
 if [ "" != "$1" ] ; then
   SIZE=$1
   shift
 fi
-
+if [ "" != "$1" ] ; then
+  BSIZE=$1
+  shift
+fi
 if [ "" != "$1" ] ; then
   STRIDE=$1
   shift
 fi
 
-BSIZE=$((SIZE / FRACT))
-BATCH=${STRIDE}
-
 cat /dev/null > ${FILE}
+
+BATCH=${STRIDE}
 while [[ ${BATCH} -le ${BSIZE} ]] ; do
   env CHECK=1 ${TRY} \
   ./multi-dgemm.sh ${SIZE} ${BATCH} $* >> ${FILE}
