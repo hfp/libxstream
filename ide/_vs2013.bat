1a3965258199f6c0378fdcb8e4f490add6265844
Hans Pabst <hans.pabst@intel.com>
2015-08-09 14:09:24 +0200

    Allow launching a specific VS version.

--- ide/_vs2013.bat
+++ ide/_vs2013.bat
@@ -4,6 +4,10 @@ SETLOCAL
 SET LIBXSTREAM_ROOT=%~d0%~p0\..
 
 CALL %~d0"%~p0"_vs.bat vs2013
-START %~d0"%~p0"_vs2013.sln
+IF NOT "%VS_IDE%" == "" (
+  START "" "%VS_IDE%" %~d0"%~p0"_vs2013.sln
+) ELSE (
+  START %~d0"%~p0"_vs2013.sln
+)
 
 ENDLOCAL
--- ide/_vs2015.bat
+++ ide/_vs2015.bat
@@ -4,6 +4,10 @@ SETLOCAL
 SET LIBXSTREAM_ROOT=%~d0%~p0\..
 
 CALL %~d0"%~p0"_vs.bat vs2015
-START %~d0"%~p0"_vs2015.sln
+IF NOT "%VS_IDE%" == "" (
+  START "" "%VS_IDE%" %~d0"%~p0"_vs2015.sln
+) ELSE (
+  START %~d0"%~p0"_vs2015.sln
+)
 
 ENDLOCAL
