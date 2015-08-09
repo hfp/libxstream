47a3b199af1363f6b5b7b8b7acb72e56a4aca86a
Hans Pabst <hans.pabst@intel.com>
2015-08-09 14:21:15 +0200

    Fixed script syntax for launching VS IDE.

--- ide/_vs2013.bat
+++ ide/_vs2013.bat
@@ -1,11 +1,11 @@
 @ECHO OFF
 SETLOCAL
 
-SET LIBXSTREAM_ROOT=%~d0%~p0\..
+SET LIBXSTREAM_ROOT=%~d0%~p0..
 
 CALL %~d0"%~p0"_vs.bat vs2013
 IF NOT "%VS_IDE%" == "" (
-  START "" "%VS_IDE%" %~d0"%~p0"_vs2013.sln
+  START "" "%VS_IDE%" "%~d0%~p0_vs2013.sln"
 ) ELSE (
   START %~d0"%~p0"_vs2013.sln
 )
