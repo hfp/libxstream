cd66bc5df584a501c7e2aaeb676591cbcbcbd615
Hans Pabst <hans.pabst@intel.com>
2024-02-26 13:34:53 +0100

    Adjusted default.

--- opencl/smm/tune_multiply.py
+++ opencl/smm/tune_multiply.py
@@ -641,7 +641,7 @@ if __name__ == "__main__":
         "-x",
         "--csv-nogflops",
         action="store_true",
-        default=False,
+        default=True,
         dest="nogflops",
         help="Exclude real GFLOPS",
     )
