1de6dc3cd8fa237456f291ec86df83374f075efd
Hans Pabst <hans.pabst@intel.com>
2026-05-20 17:15:48 +0200

    Missing headers

--- samples/smm/smm_acc.c
+++ samples/smm/smm_acc.c
@@ -13,8 +13,7 @@
 # include "smm_kernels.h"
 # include "acc_bench.h"
 # include <libxs_predict.h>
-# include <libxs_timer.h>
-# include <libxs_math.h>
+# include <libxs_str.h>
 # include <ctype.h>
 
 # if !defined(OPENCL_LIBSMM_KERNELNAME_TRANS)
