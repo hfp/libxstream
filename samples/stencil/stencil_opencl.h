2a7715586117f996b6acc1d3e344ae3b887b7a83
Hans Pabst <hans.pabst@intel.com>
2026-06-16 22:31:39 +0200

    Stencil: trim accuracy as opt-in

--- samples/stencil/stencil_opencl.h
+++ samples/stencil/stencil_opencl.h
@@ -41,6 +41,7 @@ typedef struct {
   int r_per_step;
   int sg;
   int grf256;
+  int trim;
 } stencil_opencl_key_t;
 
 typedef struct {
@@ -62,6 +63,7 @@ typedef struct {
   int r_per_step;
   int sg;
   int grf256;
+  int trim;
   int dpas;
   int verbosity;
 } stencil_context_t;
