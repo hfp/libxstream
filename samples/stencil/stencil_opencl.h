d77bd2ca77e48cb6228450861a25f3983360f965
Hans Pabst <hans.pabst@intel.com>
2026-06-24 13:34:47 +0200

    Introduced INT8-stencil

--- samples/stencil/stencil_opencl.h
+++ samples/stencil/stencil_opencl.h
@@ -29,6 +29,7 @@
 #define STENCIL_N_STRIPS (STENCIL_N_TOTAL / STENCIL_XMX_N)
 #define STENCIL_STRIPS_PER_WG 2
 #define STENCIL_N_STRIP_GROUPS (STENCIL_N_STRIPS / STENCIL_STRIPS_PER_WG)
+#define STENCIL_K_PAD_I8 64
 #define STENCIL_SG 16
 
 
@@ -50,6 +51,7 @@ typedef struct {
   int nterms;
   int lu;
   int fp32;
+  int int8;
   int bf16s;
   int blocked;
 } stencil_opencl_key_t;
@@ -62,6 +64,7 @@ typedef struct {
 
 typedef struct {
   void* dk[3];
+  void* dk_scale;
   void* coeff;
   libxstream_stream_t* stream;
   int nblocks[3];
@@ -76,6 +79,7 @@ typedef struct {
   int nterms;
   int lu;
   int fp32;
+  int int8;
   int bf16s;
   int blocked;
   int dpas;
